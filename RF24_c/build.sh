d99094429ce27110a79f250bfc1c3d40c4d91d95
Raphael Estrada <galaktor@gmx.de>
2014-01-03 12:35:36 +0000

    add "ldconfig" step to build script

--- RF24_c/build.sh
+++ RF24_c/build.sh
@@ -11,3 +11,8 @@ rflibdir=$rfhdrdir
 g++ -g -O2 -fPIC -I$rfhdrdir -I. -L$rflibdir  -lrf24 -shared -o librf24_c.so *.cpp -ansi -pedantic
 
 cp librf24_c.so /usr/local/lib
+
+# pick up new shared objects
+# will only work if /usr/local/lib is in /etc/ld.so.conf!
+# see README for details
+ldconfig
