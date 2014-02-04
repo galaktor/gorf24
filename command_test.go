a9d6742e47dbb74d5da69e90d7fae86eeb1e4951
Raphael Estrada <galaktor@gmx.de>
2014-02-04 13:17:34 +0000

    add tests for Byte() function

--- command_test.go
+++ command_test.go
@@ -16,3 +16,65 @@ func b(bits string) byte {
 	return byte(i)
 }
 
+func TestByte_Zero_ReturnsZero(t *testing.T) {
+	c := gorf24.Command(b("00000000"))
+	expected := b("00000000")
+
+	result := c.Byte()
+
+	if result != expected {
+		t.Errorf("expected '%b' but found '%b' with command '%b'", expected, result, c)
+	}
+}
+
+func TestByte_AllOnes_ReturnsSame(t *testing.T) {
+	c := gorf24.Command(b("11111111"))
+	expected := b("11111111")
+
+	result := c.Byte()
+
+	if result != expected {
+		t.Errorf("expected '%b' but found '%b' with command '%b'", expected, result, c)
+	}
+}
+
+func TestByte_LSByteZero_ReturnsSame(t *testing.T) {
+	c := gorf24.Command(b("11110000"))
+	expected := b("11110000")
+
+	result := c.Byte()
+
+	if result != expected {
+		t.Errorf("expected '%b' but found '%b' with command '%b'", expected, result, c)
+	}
+}
+
+func TestByte_MSByteZero_ReturnsSame(t *testing.T) {
+	c := gorf24.Command(b("00001111"))
+	expected := b("00001111")
+
+	result := c.Byte()
+
+	if result != expected {
+		t.Errorf("expected '%b' but found '%b' with command '%b'", expected, result, c)
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
