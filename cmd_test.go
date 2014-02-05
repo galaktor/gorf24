f358e6e514340309d2b8ecec5d5dea4e9cf84d5a
Raphael Estrada <galaktor@gmx.de>
2014-02-05 12:58:30 +0000

    new file for command tests

--- cmd_test.go
+++ cmd_test.go
@@ -50,21 +50,46 @@ func TestByte_MSByteZero_ReturnsSame(t *testing.T) {
 	}
 }
 
+func TestCMD_R_REGISTER_ZeroBits_ReturnsZero(t *testing.T) {
+	r := gorf24.Register(gorf24.B("00000000"))
+	expected := gorf24.B("00000000")
 
+	result := gorf24.CMD_R_REGISTER(r).Byte()
 
+	if result != expected {
+		t.Errorf("expected '%b' but found '%b' with register '%b'", expected, result, r)
+	}
+}
 
+func TestCMD_R_REGISTER_AllOnes_ReturnsLastFiveBits(t *testing.T) {
+	r := gorf24.Register(gorf24.B("11111111"))
+	expected := gorf24.B("00011111")
 
+	result := gorf24.CMD_R_REGISTER(r).Byte()
 
+	if result != expected {
+		t.Errorf("expected '%b' but found '%b' with register '%b'", expected, result, r)
+	}
+}
 
+func TestCMD_W_REGISTER_ZeroBits_SetsWriteRegFlag(t *testing.T) {
+	r := gorf24.Register(gorf24.B("00000000"))
+	expected := gorf24.B("00100000")
 
+	result := gorf24.CMD_W_REGISTER(r).Byte()
 
+	if result != expected {
+		t.Errorf("expected '%b' but found '%b' with register '%b'", expected, result, r)
+	}
+}
 
+func TestCMD_W_REGISTER_AllOnes_MasksTwoMSBits(t *testing.T) {
+	r := gorf24.Register(gorf24.B("11111111"))
+	expected := gorf24.B("00111111")
 
+	result := gorf24.CMD_W_REGISTER(r).Byte()
 
-
-
-
-
-
-
-
+	if result != expected {
+		t.Errorf("expected '%b' but found '%b' with register '%b'", expected, result, r)
+	}
+}
