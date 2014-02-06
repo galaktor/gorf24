d23c9bb3aa93f64a5b157de0eb6c0cda7621dc87
Raphael Estrada <galaktor@gmx.de>
2014-02-06 13:08:33 +0000

    not sure why this is failing right now...

--- register_test.go
+++ register_test.go
@@ -18,6 +18,39 @@ func TestConfigReg_SetPrimaryReader_RelevantFlagZero_SetsFlagToOne(t *testing.T)
 	}
 }
 
+func TestConfigReg_SetPrimaryReader_RelevantFlagOne_LeavesOne(t *testing.T) {
+	c := gorf24.NewConfigReg(gorf24.B("00000001"))
+	expected := gorf24.B("00000001")
+
+	c.SetPrimaryReader()
+	
+	actual := c.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with configreg '%v'", expected, actual, c)
+	}
+}
+
+func TestConfigReg_SetPrimaryReader_SomeIrrelevantFlagsFlipped_ChangesOnlyRelevantBit(t *testing.T) {
+	c := gorf24.NewConfigReg(gorf24.B("10101010"))
+	expected := gorf24.B("10101011")
+
+	c.SetPrimaryReader()
+	
+	actual := c.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with configreg '%v'", expected, actual, c)
+	}
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
