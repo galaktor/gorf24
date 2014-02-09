98c88c56bff7254fa2cb43e9af01183c024d89cc
Raphael Estrada <galaktor@gmx.de>
2014-02-09 10:34:47 +0000

    update cmd tests to recent restructuring

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -3,11 +3,13 @@ package cmd
 import (
 	"testing"
 
+	"github.com/galaktor/gorf24/reg"
+	"github.com/galaktor/gorf24/pipe"
 	"github.com/galaktor/gorf24/util"
 )
 
 func TestByte_Zero_ReturnsZero(t *testing.T) {
-	c := cmd.C(util.B("00000000"))
+	c := C(util.B("00000000"))
 	expected := util.B("00000000")
 
 	result := c.Byte()
@@ -18,7 +20,7 @@ func TestByte_Zero_ReturnsZero(t *testing.T) {
 }
 
 func TestByte_AllOnes_ReturnsSame(t *testing.T) {
-	c := cmd.C(util.B("11111111"))
+	c := C(util.B("11111111"))
 	expected := util.B("11111111")
 
 	result := c.Byte()
@@ -29,7 +31,7 @@ func TestByte_AllOnes_ReturnsSame(t *testing.T) {
 }
 
 func TestByte_LSByteZero_ReturnsSame(t *testing.T) {
-	c := cmd.C(util.B("11110000"))
+	c := C(util.B("11110000"))
 	expected := util.B("11110000")
 
 	result := c.Byte()
@@ -40,7 +42,7 @@ func TestByte_LSByteZero_ReturnsSame(t *testing.T) {
 }
 
 func TestByte_MSByteZero_ReturnsSame(t *testing.T) {
-	c := cmd.C(util.B("00001111"))
+	c := C(util.B("00001111"))
 	expected := util.B("00001111")
 
 	result := c.Byte()
@@ -50,132 +52,132 @@ func TestByte_MSByteZero_ReturnsSame(t *testing.T) {
 	}
 }
 
-func TestCMD_R_REGISTER_ZeroBits_ReturnsZero(t *testing.T) {
-	r := gorf24.Register(util.B("00000000"))
+func TestR_REGISTER_ZeroBits_ReturnsZero(t *testing.T) {
+	r := reg.Address(util.B("00000000"))
 	expected := util.B("00000000")
 
-	result := gorf24.CMD_R_REGISTER(r).Byte()
+	result := R_REGISTER(r).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with register '%b'", expected, result, r)
 	}
 }
 
-func TestCMD_R_REGISTER_AllOnes_ReturnsLastFiveBits(t *testing.T) {
-	r := gorf24.Register(util.B("11111111"))
+func TestR_REGISTER_AllOnes_ReturnsLastFiveBits(t *testing.T) {
+	r := reg.Address(util.B("11111111"))
 	expected := util.B("00011111")
 
-	result := gorf24.CMD_R_REGISTER(r).Byte()
+	result := R_REGISTER(r).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with register '%b'", expected, result, r)
 	}
 }
 
-func TestCMD_W_REGISTER_ZeroBits_SetsWriteRegFlag(t *testing.T) {
-	r := gorf24.Register(util.B("00000000"))
+func TestW_REGISTER_ZeroBits_SetsWriteRegFlag(t *testing.T) {
+	r := reg.Address(util.B("00000000"))
 	expected := util.B("00100000")
 
-	result := gorf24.CMD_W_REGISTER(r).Byte()
+	result := W_REGISTER(r).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with register '%b'", expected, result, r)
 	}
 }
 
-func TestCMD_W_REGISTER_AllOnes_MasksTwoMSBits(t *testing.T) {
-	r := gorf24.Register(util.B("11111111"))
+func TestW_REGISTER_AllOnes_MasksTwoMSBits(t *testing.T) {
+	r := reg.Address(util.B("11111111"))
 	expected := util.B("00111111")
 
-	result := gorf24.CMD_W_REGISTER(r).Byte()
+	result := W_REGISTER(r).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with register '%b'", expected, result, r)
 	}
 }
 
-func TestCMD_W_ACK_PAYLOAD_PipeZero_MaskedCorrectly(t *testing.T) {
-	p := gorf24.Pipe(0)
+func TestW_ACK_PAYLOAD_PipeZero_MaskedCorrectly(t *testing.T) {
+	p := pipe.P(0)
 	expected := util.B("10101000")
 	
-	result := gorf24.CMD_W_ACK_PAYLOAD(p).Byte()
+	result := W_ACK_PAYLOAD(p).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with pipe '%b'", expected, result, p)
 	}
 }
 
-func TestCMD_W_ACK_PAYLOAD_PipeOne_MaskedCorrectly(t *testing.T) {
-	p := gorf24.Pipe(1)
+func TestW_ACK_PAYLOAD_PipeOne_MaskedCorrectly(t *testing.T) {
+	p := pipe.P(1)
 	expected := util.B("10101001")
 	
-	result := gorf24.CMD_W_ACK_PAYLOAD(p).Byte()
+	result := W_ACK_PAYLOAD(p).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with pipe '%b'", expected, result, p)
 	}
 }
 
-func TestCMD_W_ACK_PAYLOAD_PipeTwo_MaskedCorrectly(t *testing.T) {
-	p := gorf24.Pipe(2)
+func TestW_ACK_PAYLOAD_PipeTwo_MaskedCorrectly(t *testing.T) {
+	p := pipe.P(2)
 	expected := util.B("10101010")
 	
-	result := gorf24.CMD_W_ACK_PAYLOAD(p).Byte()
+	result := W_ACK_PAYLOAD(p).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with pipe '%b'", expected, result, p)
 	}
 }
 
-func TestCMD_W_ACK_PAYLOAD_PipeThree_MaskedCorrectly(t *testing.T) {
-	p := gorf24.Pipe(3)
+func TestW_ACK_PAYLOAD_PipeThree_MaskedCorrectly(t *testing.T) {
+	p := pipe.P(3)
 	expected := util.B("10101011")
 	
-	result := gorf24.CMD_W_ACK_PAYLOAD(p).Byte()
+	result := W_ACK_PAYLOAD(p).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with pipe '%b'", expected, result, p)
 	}
 }
 
-func TestCMD_W_ACK_PAYLOAD_PipeFour_MaskedCorrectly(t *testing.T) {
-	p := gorf24.Pipe(4)
+func TestW_ACK_PAYLOAD_PipeFour_MaskedCorrectly(t *testing.T) {
+	p := pipe.P(4)
 	expected := util.B("10101100")
 	
-	result := gorf24.CMD_W_ACK_PAYLOAD(p).Byte()
+	result := W_ACK_PAYLOAD(p).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with pipe '%b'", expected, result, p)
 	}
 }
 
-func TestCMD_W_ACK_PAYLOAD_PipeFive_MaskedCorrectly(t *testing.T) {
-	p := gorf24.Pipe(5)
+func TestW_ACK_PAYLOAD_PipeFive_MaskedCorrectly(t *testing.T) {
+	p := pipe.P(5)
 	expected := util.B("10101101")
 	
-	result := gorf24.CMD_W_ACK_PAYLOAD(p).Byte()
+	result := W_ACK_PAYLOAD(p).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with pipe '%b'", expected, result, p)
 	}
 }
 
-func TestCMD_W_ACK_PAYLOAD_PipeHigherThanFive_ThreeBits_MasksCorrectlyAlthoughInvalid(t *testing.T) {
-	p := gorf24.Pipe(6)
+func TestW_ACK_PAYLOAD_PipeHigherThanFive_ThreeBits_MasksCorrectlyAlthoughInvalid(t *testing.T) {
+	p := pipe.P(6)
 	expected := util.B("10101110")
 	
-	result := gorf24.CMD_W_ACK_PAYLOAD(p).Byte()
+	result := W_ACK_PAYLOAD(p).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with pipe '%b'", expected, result, p)
 	}
 }
 
-func TestCMD_W_ACK_PAYLOAD_PipeHigherThanFive_OverThreeBits_TruncatesAndMasksCorrectlyAlthoughInvalid(t *testing.T) {
-	p := gorf24.Pipe(10)
+func TestW_ACK_PAYLOAD_PipeHigherThanFive_OverThreeBits_TruncatesAndMasksCorrectlyAlthoughInvalid(t *testing.T) {
+	p := pipe.P(10)
 	expected := util.B("10101010")
 	
-	result := gorf24.CMD_W_ACK_PAYLOAD(p).Byte()
+	result := W_ACK_PAYLOAD(p).Byte()
 
 	if result != expected {
 		t.Errorf("expected '%b' but found '%b' with pipe '%b'", expected, result, p)
