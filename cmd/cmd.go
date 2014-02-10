6f0186896dbb9d75bcbad6ac48429720675bb541
Raphael Estrada <galaktor@gmx.de>
2014-02-10 08:17:41 +0000

    make cmd bits constants

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"github.com/galaktor/gorf24/reg"
 	"github.com/galaktor/gorf24/pipe"
-	"github.com/galaktor/gorf24/util"
 )
 
 type C byte
@@ -12,18 +11,18 @@ func (c C) Byte() byte {
 	return byte(c)
 }
 
-var (
-	NOP = C(util.B("11111111"))
+const (
+	NOP          = C(0xFF) // 1111 1111
 	// R_REGISTER - is a function, see below
 	// W_REGSITER - is a function, see below
-	R_RX_PAYLOAD = C(util.B("01100001"))
-	W_TX_PAYLOAD = C(util.B("10100000"))
-	FLUSH_TX     = C(util.B("11100001"))
-	FLUSH_RX     = C(util.B("11100010"))
-	REUSE_TX_PL  = C(util.B("11100011"))
-	ACTIVATE     = C(util.B("01010000"))
+	R_RX_PAYLOAD = C(0x61) // 0110 0001
+	W_TX_PAYLOAD = C(0xA0) // 1010 0000
+	FLUSH_TX     = C(0xE1) // 1110 0001
+	FLUSH_RX     = C(0xE2) // 1110 0010
+	REUSE_TX_PL  = C(0xE3) // 1110 0011
+	ACTIVATE     = C(0x50) // 0101 0000
 	// W_ACK_PAYLOAD - is a function, see below
-	R_RX_PL_WID         = C(util.B("01100000"))
+	R_RX_PL_WID  = C(0x60) // 0110 0000
 )
 
 func R_REGISTER(r reg.R) C {
@@ -37,3 +36,5 @@ func W_REGISTER(r reg.R) C {
 func W_ACK_PAYLOAD(p pipe.P) C {
 	return C(0xA8 | p)
 }
+
+
