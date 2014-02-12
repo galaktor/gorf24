40330701dc564c7af3da2e42f1aeb5fa00cd94e6
Raphael Estrada <galaktor@gmx.de>
2014-02-12 12:19:15 +0000

    add util helpers to convert more than one byte

--- util/util.go
+++ util/util.go
@@ -12,3 +12,30 @@ func B(bits string) byte {
 	i, _ := strconv.ParseUint(bits, 2, 8)
 	return byte(i)
 }
+
+func B4(bits string) uint32 {
+	i, _ := strconv.ParseUint(bits, 2, 32)
+	return uint32(i)
+}
+
+func B5(bits string) [5]byte {
+	i, _ := strconv.ParseUint(bits, 2, 64)
+
+	return [5]byte {
+		byte(i >> 32),
+		byte(i >> 24),
+		byte(i >> 16),
+		byte(i >> 8),
+		byte(i),
+	}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ util/util_test.go
@@ -0,0 +1,132 @@
+package util
+
+import (
+	"testing"
+)
+
+func TestB_Zeroes_ReturnsZero(t *testing.T) {
+	expected := byte(0)
+	
+	actual := B("00000000")
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
+
+func TestB_Ones_ReturnsMaxByte(t *testing.T) {
+	expected := byte(0xFF)
+	
+	actual := B("11111111")
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
+
+func TestB_MixedZeroesAndOnes_ReturnsRightNumber(t *testing.T) {
+	expected := byte(0xAA)
+	
+	actual := B("10101010")
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
+
+func TestB4_Zeroes_ReturnsZero(t *testing.T) {
+	expected := uint32(0)
+
+	actual := B4("00000000000000000000000000000000")
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
+
+func TestB4_Ones_ReturnsMaxUint32(t *testing.T) {
+	expected := uint32(0xFFFFFFFF)
+
+	actual := B4("11111111111111111111111111111111")
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
+
+func TestB4_MixesZeroesAndOnes_ReturnsRightNumber(t *testing.T) {
+	expected := uint32(0xAAAAAAAA)
+
+	actual := B4("10101010101010101010101010101010")
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
+
+func TestB5_Zeroes_ReturnsZeroes(t *testing.T) {
+	expected := [5]byte{0, 0, 0, 0, 0}
+
+	actual := B5("0000000000000000000000000000000000000000")
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v'", expected, actual)
+	}
+}
+
+func TestB5_Ones_ReturnsMaxBytes(t *testing.T) {
+	expected := [5]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
+
+	actual := B5("1111111111111111111111111111111111111111")
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v'", expected, actual)
+	}
+}
+
+func TestB5_MixedZeroesAndOnes_ReturnsRightBytes(t *testing.T) {
+	expected := [5]byte{0xAA, 0xAA, 0xAA, 0xAA, 0xAA}
+
+	actual := B5("1010101010101010101010101010101010101010")
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v'", expected, actual)
+	}
+}
+
+func TestB5_LeadingZeroes_ReturnsRightBytes(t *testing.T) {
+	expected := [5]byte{0x00, 0x00, 0xAA, 0xAA, 0xAA}
+
+	actual := B5("0000000000000000101010101010101010101010")
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v'", expected, actual)
+	}
+}
+
+func TestB5_TrailingZeroes_ReturnsRightBytes(t *testing.T) {
+	expected := [5]byte{0xAA, 0xAA, 0xAA, 0x00, 0x00}
+
+	actual := B5("1010101010101010101010100000000000000000")
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v'", expected, actual)
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
