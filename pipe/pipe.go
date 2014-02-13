96e26ba02aa4db364d47721f6cc4ac19cd0b1d7a
Raphael Estrada <galaktor@gmx.de>
2014-02-13 12:37:06 +0000

    reminder comment to consider using bytes for pipe const values

--- pipe/pipe.go
+++ pipe/pipe.go
@@ -3,6 +3,9 @@ package pipe
 // 0 through 5, giving 6 data pipe ids
 type P byte
 
+// TODO: consider setting pipe constants to bits as used in autoack etc regss
+// can then just do "return flags & p == p"?
+
 const (
 	P0 P = iota
 	P1
