7e045d4c9938b4398a3f8e5e881a6cdc962038e9
Raphael Estrada <galaktor@gmx.de>
2014-02-13 12:48:11 +0000

    IsEnabled logic and tests

--- reg/enrxaddr.go
+++ reg/enrxaddr.go
@@ -10,13 +10,27 @@ type EnabledRxAddresses struct {
 }
 
 func NewEnabledRxAddresses(flags byte) *EnabledRxAddresses {
-	return &EnabledRxAddresses{R{addr.EN_RXADDR,0}}
+	return &EnabledRxAddresses{R{addr.EN_RXADDR,flags}}
 }
 
+/* ERX_Px */
 func (e *EnabledRxAddresses) IsEnabled(p pipe.P) bool {
-	return true // TODO
-}
+	var result bool
 
-func (e *EnabledRxAddresses) SetEnabled(p pipe.P, enabled bool) {
+	switch p {
+	case pipe.P0: result = e.flags & 1 == 1
+	case pipe.P1: result = e.flags & 2 == 2
+	case pipe.P2: result = e.flags & 4 == 4
+	case pipe.P3: result = e.flags & 8 == 8
+	case pipe.P4: result = e.flags & 16 == 16
+	case pipe.P5: result = e.flags & 32 == 32
+	}
+
+	return result
+}
+func (e *EnabledRxAddresses) Enable(p pipe.P) {
+	// TODO
+}
+func (e *EnabledRxAddresses) Disable(p pipe.P) {
 	// TODO
 }
--- reg/enrxaddr_test.go
+++ reg/enrxaddr_test.go
@@ -1,6 +1,149 @@
 package reg
 
 import (
+	"testing"
 
+	"github.com/galaktor/gorf24/util"
+	"github.com/galaktor/gorf24/pipe"
 )
+
+func TestIsEnabled_Pipe0_FlagZero_ReturnsFalse(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("11111110"))
+	expected := false
+
+	actual := e.IsEnabled(pipe.P0)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe0_FlagOne_ReturnsTrue(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("00000001"))
+	expected := true
+
+	actual := e.IsEnabled(pipe.P0)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe1_FlagZero_ReturnsFalse(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("11111101"))
+	expected := false
+
+	actual := e.IsEnabled(pipe.P1)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe1_FlagOne_ReturnsTrue(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("00000010"))
+	expected := true
+
+	actual := e.IsEnabled(pipe.P1)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe2_FlagZero_ReturnsFalse(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("11111011"))
+	expected := false
+
+	actual := e.IsEnabled(pipe.P2)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe2_FlagOne_ReturnsTrue(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("00000100"))
+	expected := true
+
+	actual := e.IsEnabled(pipe.P2)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe3_FlagZero_ReturnsFalse(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("11110111"))
+	expected := false
+
+	actual := e.IsEnabled(pipe.P3)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe3_FlagOne_ReturnsTrue(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("00001000"))
+	expected := true
+
+	actual := e.IsEnabled(pipe.P3)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe4_FlagZero_ReturnsFalse(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("11101111"))
+	expected := false
+
+	actual := e.IsEnabled(pipe.P4)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe4_FlagOne_ReturnsTrue(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("00010000"))
+	expected := true
+
+	actual := e.IsEnabled(pipe.P4)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe5_FlagZero_ReturnsFalse(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("11011111"))
+	expected := false
+
+	actual := e.IsEnabled(pipe.P5)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+func TestIsEnabled_Pipe5_FlagOne_ReturnsTrue(t *testing.T) {
+	e := NewEnabledRxAddresses(util.B("00100000"))
+	expected := true
+
+	actual := e.IsEnabled(pipe.P5)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with enRxaddr '%v'", expected, actual, e)
+	}
+}
+
+
+
+
+
+
+
+
 
