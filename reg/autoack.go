8b9397602991717aa63e283a9d66eadcd2e58106
Raphael Estrada <galaktor@gmx.de>
2014-02-13 08:21:58 +0000

    Autoack IsEnabled getters with tests

--- reg/autoack.go
+++ reg/autoack.go
@@ -2,6 +2,7 @@ package reg
 
 import (
 	"github.com/galaktor/gorf24/reg/addr"
+	"github.com/galaktor/gorf24/pipe"
 )
 
 type AutoAck struct {
@@ -10,4 +11,20 @@ type AutoAck struct {
 
 func NewAutoAck(flags byte) *AutoAck {
 	return &AutoAck{R{a: addr.EN_AA, flags: flags}}
+}
+
+/* ENAA_Px */
+func (a *AutoAck) IsEnabled(p pipe.P) bool {
+	var result bool
+
+	switch p {
+	case pipe.P0: result = a.flags & 1 == 1
+	case pipe.P1: result = a.flags & 2 == 2
+	case pipe.P2: result = a.flags & 4 == 4
+	case pipe.P3: result = a.flags & 8 == 8
+	case pipe.P4: result = a.flags & 16 == 16
+	case pipe.P5: result = a.flags & 32 == 32
+	}
+
+	return result
 }
--- reg/autoack_test.go
+++ reg/autoack_test.go
@@ -1,21 +1,152 @@
 package reg
 
 import (
-//	"testing"
+	"testing"
 
-//	"github.com/galaktor/gorf24/util"
+	"github.com/galaktor/gorf24/pipe"
+	"github.com/galaktor/gorf24/util"
 )
 
-/*
-func Test(t *testing.T) {
-	//c := NewConfigReg(util.B("00000000"))
-	expected := util.B("00000001")
 
-	// TODO act
+func TestIsEnabled_Pipe0_Disabled_ReturnsFalse(t *testing.T) {
+	a := NewAutoAck(util.B("11111110"))
+	expected := false
 
-	actual := c.Byte()
+	actual := a.IsEnabled(pipe.P0)
+
 	if actual != expected {
-		t.Errorf("expected '%b' but found '%b' with configreg '%v'", expected, actual, c)
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
 	}
 }
-*/
+
+func TestIsEnabled_Pipe0_Enabled_ReturnsTrue(t *testing.T) {
+	a := NewAutoAck(util.B("00000001"))
+	expected := true
+
+	actual := a.IsEnabled(pipe.P0)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe1_Disabled_ReturnsFalse(t *testing.T) {
+	a := NewAutoAck(util.B("11111101"))
+	expected := false
+
+	actual := a.IsEnabled(pipe.P1)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe1_Enabled_ReturnsTrue(t *testing.T) {
+	a := NewAutoAck(util.B("00000010"))
+	expected := true
+
+	actual := a.IsEnabled(pipe.P1)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe2_Disabled_ReturnsFalse(t *testing.T) {
+	a := NewAutoAck(util.B("11111011"))
+	expected := false
+
+	actual := a.IsEnabled(pipe.P2)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe2_Enabled_ReturnsTrue(t *testing.T) {
+	a := NewAutoAck(util.B("00000100"))
+	expected := true
+
+	actual := a.IsEnabled(pipe.P2)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe3_Disabled_ReturnsFalse(t *testing.T) {
+	a := NewAutoAck(util.B("11110111"))
+	expected := false
+
+	actual := a.IsEnabled(pipe.P3)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe3_Enabled_ReturnsTrue(t *testing.T) {
+	a := NewAutoAck(util.B("00001000"))
+	expected := true
+
+	actual := a.IsEnabled(pipe.P3)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe4_Disabled_ReturnsFalse(t *testing.T) {
+	a := NewAutoAck(util.B("11101111"))
+	expected := false
+
+	actual := a.IsEnabled(pipe.P4)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe4_Enabled_ReturnsTrue(t *testing.T) {
+	a := NewAutoAck(util.B("00010000"))
+	expected := true
+
+	actual := a.IsEnabled(pipe.P4)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe5_Disabled_ReturnsFalse(t *testing.T) {
+	a := NewAutoAck(util.B("11011111"))
+	expected := false
+
+	actual := a.IsEnabled(pipe.P5)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe5_Enabled_ReturnsTrue(t *testing.T) {
+	a := NewAutoAck(util.B("00100000"))
+	expected := true
+
+	actual := a.IsEnabled(pipe.P5)
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
+
+func TestIsEnabled_Pipe6_InvalidButBitIsFlipped_ReturnsFalseAnyway(t *testing.T) {
+	a := NewAutoAck(util.B("01000000"))
+	expected := false
+
+	actual := a.IsEnabled(pipe.P(6))
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with autoack '%v'", expected, actual, a)
+	}
+}
