e3c4e1529de098741bf94a449f112b036cf09ab2
Raphael Estrada <galaktor@gmx.de>
2014-02-14 22:09:52 +0000

    add comment with spec excerpts

--- reg/transobserve.go
+++ reg/transobserve.go
@@ -12,12 +12,17 @@ func NewTransObserve(flags byte) *TransObserve {
 	return &TransObserve{R{a: addr.OBSERVE_TX, flags: flags}}
 }
 
-/* ARC_CNT (bits 3:0) */
+/* ARC_CNT (bits 3:0) 
+   Count retransmitted packets. The counter is reset
+   when transmission of a new packet starts.*/
 func (o *TransObserve) RetransmittedPacketCount() uint8 {
 	return o.flags & 0x0F
 }
 
-/* PLOS_CNT (bits 7:4) */
+/* PLOS_CNT (bits 7:4) 
+   Count lost packets. The counter is overflow pro-
+   tected to 15, and discontinues at max until reset.
+   The counter is reset by writing to RF_CH .*/
 func (o *TransObserve) LostPacketCount() uint8 {
 	return (o.flags & 0xF0) >> 4
 }
