e5fe494e29819d478e706326f89d40a2961ad5ce
Raphael Estrada <galaktor@gmx.de>
2014-02-14 16:52:04 +0000

    rfchannel set with errors

--- reg/rfchannel.go
+++ reg/rfchannel.go
@@ -1,6 +1,9 @@
 package reg
 
 import (
+	"errors"
+	"fmt"
+	
 	"github.com/galaktor/gorf24/reg/addr"
 )
 
@@ -20,3 +23,21 @@ func NewRfChannel(flags byte) *RfChannel {
 func (c *RfChannel) Get() uint8 {
 	return c.flags & 0x7F // ignore bit 7
 }
+func (c *RfChannel) Set(ch uint8) error {
+	if ch > 127 {
+		return errors.New(fmt.Sprintf("value outside of legal range: %v. Only values 1 - 127 allowed.", ch))
+	}
+	
+	c.flags = ch & 0x7F
+	return nil
+}
+
+
+
+
+
+
+
+
+
+
--- reg/rfchannel_test.go
+++ reg/rfchannel_test.go
@@ -39,22 +39,65 @@ func TestGet_FortyTwo_ReturnsFortyTwo(t *testing.T) {
 	}
 }
 
+func TestSet_Zero_FlipsRightBits(t *testing.T) {
+	c := NewRfChannel(util.B("11111111"))
+	expected := util.B("00000000")
 
+	err := c.Set(0)
 
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+		t.FailNow()
+	}
 
+	actual := c.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' on rfchan '%v'", expected, actual, c)
+	}
+}
 
+func TestSet_FortyTwo_FlipsRightBits(t *testing.T) {
+	c := NewRfChannel(util.B("00000000"))
+	expected := byte(42)
 
+	err := c.Set(42)
 
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+		t.FailNow()
+	}
 
+	actual := c.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' on rfchan '%v'", expected, actual, c)
+	}
+}
 
+func TestSet_127_FlipsRightBits(t *testing.T) {
+	c := NewRfChannel(util.B("00000000"))
+	expected := byte(util.B("01111111"))
 
+	err := c.Set(127)
 
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+		t.FailNow()
+	}
 
+	actual := c.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' on rfchan '%v'", expected, actual, c)
+	}
+}
 
+func TestSet_128_ReturnsError(t *testing.T) {
+	c := NewRfChannel(util.B("00000000"))
+	expected := "value outside of legal range: 128. Only values 1 - 127 allowed."
 
+	err := c.Set(128)
 
-
-
-
-
-
+	actual := err.Error()
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' on rfchan '%v'", expected, actual, c)
+	}
+}
