74f7c7b87558b8da00b68573e29a5858c8dbbabd
Raphael Estrada <galaktor@gmx.de>
2014-02-15 15:22:59 +0000

    rpd register with tests for triggered RPD

--- reg/rpd.go
+++ reg/rpd.go
@@ -4,10 +4,20 @@ import (
 	"github.com/galaktor/gorf24/reg/addr"
 )
 
+// bits 7:1 reserved
+
 type ReceivedPowerDetector struct {
 	R
 }
 
 func NewRPD(flags byte) *ReceivedPowerDetector {
 	return &ReceivedPowerDetector{R{a: addr.RPD, flags: flags}}
+}
+
+/* RPD (bit 0) 
+   triggers at received power levels above -64 dBm that
+   are present in the RF channel you receive on. If the
+   received power is less than -64 dBm, [sic] RDP = 0.*/
+func (r *ReceivedPowerDetector) Triggered() bool {
+	return r.flags & 1 == 1
 }
---
+++ reg/rpd_test.go
@@ -0,0 +1,41 @@
+package reg
+
+import (
+	"testing"
+
+	"github.com/galaktor/gorf24/reg/addr"
+	"github.com/galaktor/gorf24/util"
+)
+
+func TestNewRPD_RegisterAddress_IsRPD(t *testing.T) {
+	r := NewRPD(0)
+	expected := addr.RPD
+
+	actual := r.Address()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with rpd '%v'", expected, actual, r)
+	}
+}
+
+func TestTriggered_BitZero_ReturnsFalse(t *testing.T) {
+	r := NewRPD(util.B("11111110"))
+	expected := false
+
+	actual := r.Triggered()
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with rpd '%v'", expected, actual, r)
+	}
+}
+
+func TestTriggered_BitOne_ReturnsTrue(t *testing.T) {
+	r := NewRPD(util.B("00000001"))
+	expected := true
+
+	actual := r.Triggered()
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with rpd '%v'", expected, actual, r)
+	}
+}
