58e2ac6a9525487ae2fa6c009cca692636fca372
Raphael Estrada <galaktor@gmx.de>
2014-02-17 17:47:22 +0000

    feature register with tests; missing one flag

--- reg/feature.go
+++ reg/feature.go
@@ -4,10 +4,38 @@ import (
 	"github.com/galaktor/gorf24/reg/addr"
 )
 
+// bits 7:3 reserved
+
 type Feature struct {
 	R
 }
 
 func NewFeature(flags byte) *Feature {
 	return &Feature{R{a: addr.FEATURE, flags: flags}}
+}
+
+/* EN_DYN_ACK (bit 0)
+   Enables the W_TX_PAYLOAD_NOACK command */
+func (f *Feature) IsDynamicAckEnabled() bool {
+	return f.flags & 0x01 == 0x01
+}
+func (f *Feature) SetDynamicAck(enabled bool) {
+	if enabled {
+		f.flags |= 0x01
+	} else {
+		f.flags &= 0xFE
+	}
+}
+
+/* EN_ACK_PAY (bit 1)
+   Enables Payload with ACK */
+func (f *Feature) IsPayloadWithAckEnabled() bool {
+	return f.flags & 0x02 == 0x02
+}
+func (f *Feature) SetPayloadWithAck(enabled bool) {
+	if enabled {
+		f.flags |= 0x02
+	} else {
+		f.flags &= 0xFD
+	}
 }
---
+++ reg/feature_test.go
@@ -0,0 +1,120 @@
+package reg
+
+import (
+	"testing"
+	
+	"github.com/galaktor/gorf24/reg/addr"
+	"github.com/galaktor/gorf24/util"
+)
+
+func TestNewFeature_RegisterAddress_IsFEATURE(t *testing.T) {
+	f := NewFeature(0)
+	expected := addr.FEATURE
+
+	actual := f.Address()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with feature '%v'", expected, actual, f)
+	}
+}
+
+func TestSetDynamicAck_Enabled_FlipsRightBit(t *testing.T) {
+	f := NewFeature(util.B("00000000"))
+	expected := util.B("00000001")
+
+	f.SetDynamicAck(true)
+
+	actual := f.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with feature '%v'", expected, actual, f)
+	}
+}
+
+func TestSetDynamicAck_Disabled_FlipsRightBit(t *testing.T) {
+	f := NewFeature(util.B("11111111"))
+	expected := util.B("11111110")
+
+	f.SetDynamicAck(false)
+
+	actual := f.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with feature '%v'", expected, actual, f)
+	}
+}
+
+func TestIsDynamicAckEnabled_BitZero_ReturnsFalse(t *testing.T) {
+	f := NewFeature(util.B("11111110"))
+	expected := false
+
+	actual := f.IsDynamicAckEnabled()
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with feature '%v'", expected, actual, f)
+	}
+}
+
+func TestIsDynamicAckEnabled_BitOne_ReturnsTrue(t *testing.T) {
+	f := NewFeature(util.B("00000001"))
+	expected := true
+
+	actual := f.IsDynamicAckEnabled()
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with feature '%v'", expected, actual, f)
+	}
+}
+
+
+
+
+
+
+
+func TestSetPayloadWithAck_Enabled_FlipsRightBit(t *testing.T) {
+	f := NewFeature(util.B("00000010"))
+	expected := util.B("00000010")
+
+	f.SetPayloadWithAck(true)
+
+	actual := f.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with feature '%v'", expected, actual, f)
+	}
+}
+
+func TestSetPayloadWithAck_Disabled_FlipsRightBit(t *testing.T) {
+	f := NewFeature(util.B("11111111"))
+	expected := util.B("11111101")
+
+	f.SetPayloadWithAck(false)
+
+	actual := f.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with feature '%v'", expected, actual, f)
+	}
+}
+
+func TestIsPayloadWithAckEnabled_BitZero_ReturnsFalse(t *testing.T) {
+	f := NewFeature(util.B("11111101"))
+	expected := false
+
+	actual := f.IsPayloadWithAckEnabled()
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with feature '%v'", expected, actual, f)
+	}
+}
+
+func TestIsPayloadWithAckEnabled_BitOne_ReturnsTrue(t *testing.T) {
+	f := NewFeature(util.B("00000010"))
+	expected := true
+
+	actual := f.IsPayloadWithAckEnabled()
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with feature '%v'", expected, actual, f)
+	}
+}
+
+// TODO: EN_DPL
+
