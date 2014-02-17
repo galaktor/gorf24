4a3bfa507e6c5ab8633834e848cc7137021659af
Raphael Estrada <galaktor@gmx.de>
2014-02-17 12:56:47 +0000

    fifo status Rx and Tx full/partial/empty with tests

--- reg/fifo.go
+++ reg/fifo.go
@@ -4,10 +4,41 @@ import (
 	"github.com/galaktor/gorf24/reg/addr"
 )
 
+// bit 7 reserved
+// bits 3:2 reserved
+
+type FifoUsage byte
+const (
+	FIFO_PARTIAL FifoUsage = iota // 0x0
+	FIFO_EMPTY // 0x1
+	FIFO_FULL  // 0x2
+	FIFO_INVALID // 0x3
+)
+
 type FifoStatus struct {
 	R
 }
 
 func NewFifoStatus(flags byte) *FifoStatus {
 	return &FifoStatus{R{a: addr.FIFO_STATUS, flags: flags}}
+}
+
+/* RX_EMPTY (bit 0)
+   RX_FULL  (bit 1) */
+func (f *FifoStatus) Rx() FifoUsage {
+	// xxxxxx00 -> partial
+	// xxxxxx01 -> empty
+	// xxxxxx10 -> full
+	// xxxxxx11 -> empty AND full? INVALID!
+	return FifoUsage(f.flags & 3)
+}
+
+/* TX_EMPTY (bit 4)
+   TX_FULL  (bit 5) */
+func (f *FifoStatus) Tx() FifoUsage {
+	// xx00xxxx -> partial
+	// xx01xxxx -> empty
+	// xx10xxxx -> full
+	// xx11xxxx -> empty AND full? INVALID!
+	return FifoUsage((f.flags >> 4) & 3)
 }
--- reg/fifo_test.go
+++ reg/fifo_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/galaktor/gorf24/reg/addr"
+	"github.com/galaktor/gorf24/util"
 )
 
 func TestNewFifoStatus_RegisterAddress_IsFIFO_STATUS(t *testing.T) {
@@ -11,6 +12,94 @@ func TestNewFifoStatus_RegisterAddress_IsFIFO_STATUS(t *testing.T) {
 	expected := addr.FIFO_STATUS
 
 	actual := f.Address()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with fifostatus '%v'", expected, actual, f)
+	}
+}
+
+func TestRx_FullBitOne_ReturnsFIFO_FULL(t *testing.T) {
+	f := NewFifoStatus(util.B("00000010"))
+	expected := FIFO_FULL
+
+	actual := f.Rx()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with fifostatus '%v'", expected, actual, f)
+	}
+}
+
+func TestRx_EmptyBitOne_ReturnsFIFO_EMPTY(t *testing.T) {
+	f := NewFifoStatus(util.B("00000001"))
+	expected := FIFO_EMPTY
+
+	actual := f.Rx()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with fifostatus '%v'", expected, actual, f)
+	}
+}
+
+func TestRx_EmptyAndFullBitsZero_ReturnsFIFO_PARTIAL(t *testing.T) {
+	f := NewFifoStatus(util.B("11111100"))
+	expected := FIFO_PARTIAL
+
+	actual := f.Rx()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with fifostatus '%v'", expected, actual, f)
+	}
+}
+
+func TestRx_EmptyAndFullBitsOne_ReturnsFIFO_INVALID(t *testing.T) {
+	f := NewFifoStatus(util.B("00000011"))
+	expected := FIFO_INVALID
+
+	actual := f.Rx()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with fifostatus '%v'", expected, actual, f)
+	}
+}
+
+func TestTx_FullBitOne_ReturnsFIFO_FULL(t *testing.T) {
+	f := NewFifoStatus(util.B("00100000"))
+	expected := FIFO_FULL
+
+	actual := f.Tx()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with fifostatus '%v'", expected, actual, f)
+	}
+}
+
+func TestTx_EmptyBitOne_ReturnsFIFO_EMPTY(t *testing.T) {
+	f := NewFifoStatus(util.B("00010000"))
+	expected := FIFO_EMPTY
+
+	actual := f.Tx()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with fifostatus '%v'", expected, actual, f)
+	}
+}
+
+func TestTx_EmptyAndFullBitsZero_ReturnsFIFO_PARTIAL(t *testing.T) {
+	f := NewFifoStatus(util.B("11001111"))
+	expected := FIFO_PARTIAL
+
+	actual := f.Tx()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with fifostatus '%v'", expected, actual, f)
+	}
+}
+
+func TestTx_EmptyAndFullBitsOne_ReturnsFIFO_INVALID(t *testing.T) {
+	f := NewFifoStatus(util.B("00110000"))
+	expected := FIFO_INVALID
+
+	actual := f.Tx()
 
 	if actual != expected {
 		t.Errorf("expected '%b' but found '%b' with fifostatus '%v'", expected, actual, f)
