e980bf8d0a81db62732567c3728846fa56bae887
Raphael Estrada <galaktor@gmx.de>
2014-02-17 08:24:07 +0000

    rx payload width with tests

--- reg/rxpw.go
+++ reg/rxpw.go
@@ -1,11 +1,16 @@
 package reg
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/galaktor/gorf24/reg/addr"
 	"github.com/galaktor/gorf24/pipe"
 )
 
-// bits 7:6 reserved
+// 
+
+const PW_MAX_WIDTH uint8 = 32
 
 type RxPayloadWidth struct {
 	R
@@ -13,4 +18,22 @@ type RxPayloadWidth struct {
 
 func NewRxPayloadWidth(p pipe.P, flags byte) *RxPayloadWidth {
 	return &RxPayloadWidth{R{a: addr.RX_PW(p), flags: flags}}
+}
+
+/* RX_PWx (bits 5:0)
+   bits 7:6 reserved, must be '00' */
+func (r *RxPayloadWidth) Set(w uint8) error {
+	if w > PW_MAX_WIDTH {
+		return errors.New(fmt.Sprintf("value out of legal range: %v. allowed values from 0 - %v", w, PW_MAX_WIDTH))
+	}
+	
+	r.flags = (r.flags & 0) | w
+	return nil
+}
+func (r *RxPayloadWidth) Get() uint8 {
+	if r.flags > PW_MAX_WIDTH {
+		return PW_MAX_WIDTH
+	}
+
+	return r.flags
 }
--- reg/rxpw_test.go
+++ reg/rxpw_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/galaktor/gorf24/pipe"
+	"github.com/galaktor/gorf24/util"
 	"github.com/galaktor/gorf24/reg/addr"
 )
 
@@ -21,12 +22,88 @@ func TestNewRxPayloadWidth_RegisterAddress_AllPipes_HasRightRegister(t *testing.
 	}
 }
 
+func TestSet_Zero_FlipsRelevantBits(t *testing.T) {
+	w := NewRxPayloadWidth(pipe.P0, util.B("00111111"))
+	expected := util.B("00000000")
 
+	w.Set(0)
+	
+	actual := w.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with rxpw '%v'", expected, actual, w)
+	}
+}
 
+func TestSet_One_FlipsRelevantBits(t *testing.T) {
+	w := NewRxPayloadWidth(pipe.P0, util.B("0000000"))
+	expected := util.B("00000001")
 
+	w.Set(1)
+	
+	actual := w.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with rxpw '%v'", expected, actual, w)
+	}
+}
 
+func TestSet_ThirtyTwo_FlipsRelevantBits(t *testing.T) {
+	w := NewRxPayloadWidth(pipe.P0, util.B("00000000"))
+	expected := util.B("00100000")
 
+	w.Set(32)
+	
+	actual := w.Byte()
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with rxpw '%v'", expected, actual, w)
+	}
+}
 
+func TestSet_OverThrityTwo_ReturnsError(t *testing.T) {
+	w := NewRxPayloadWidth(pipe.P0, util.B("00000000"))
+	expected := "value out of legal range: 33. allowed values from 0 - 32"
 
+	err := w.Set(33)
+	
+	if err == nil {
+		t.Error("expected error but found nil")
+		t.FailNow()
+	}
 
+	actual := err.Error()
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with rxpw '%v'", expected, actual, w)
+	}
+}
 
+func TestGet_Zero_ReturnsZero(t *testing.T) {
+	w := NewRxPayloadWidth(pipe.P0, util.B("00000000"))
+	expected := uint8(0)
+
+	actual := w.Get()
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with rxpw '%v'", expected, actual, w)
+	}
+}
+
+func TestGet_ThirtyTwo_ReturnsThirtyTwo(t *testing.T) {
+	w := NewRxPayloadWidth(pipe.P0, util.B("00100000"))
+	expected := uint8(32)
+
+	actual := w.Get()
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with rxpw '%v'", expected, actual, w)
+	}
+}
+
+func TestGet_OverThirtyTwo_TruncatesAndReturnsThirtyTwo(t *testing.T) {
+	w := NewRxPayloadWidth(pipe.P0, byte(33))
+	expected := uint8(32)
+
+	actual := w.Get()
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with rxpw '%v'", expected, actual, w)
+	}
+}
