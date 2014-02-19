35047290818ae107e6dcbb252312d980fa4bd916
Raphael Estrada <galaktor@gmx.de>
2014-02-19 22:02:11 +0000

    update rf24.go to match recent reg pkg moves

--- rf24.go
+++ rf24.go
@@ -9,81 +9,100 @@ import (
 	"time"
 
 	"github.com/galaktor/gorf24/cmd"
-	"github.com/galaktor/gorf24/pipe"
-	"github.com/galaktor/gorf24/reg"
 	"github.com/galaktor/gorf24/gpio"
+	"github.com/galaktor/gorf24/pipe"
 	"github.com/galaktor/gorf24/spi"
+
+	"github.com/galaktor/gorf24/reg"
+	"github.com/galaktor/gorf24/reg/xaddr"
+	/* registers */
+	"github.com/galaktor/gorf24/reg/addrw"
+	"github.com/galaktor/gorf24/reg/autoack"
+	"github.com/galaktor/gorf24/reg/config"
+	"github.com/galaktor/gorf24/reg/dynpd"
+	"github.com/galaktor/gorf24/reg/enrxaddr"
+	"github.com/galaktor/gorf24/reg/feature"
+	"github.com/galaktor/gorf24/reg/fifo"
+	"github.com/galaktor/gorf24/reg/retrans"
+	"github.com/galaktor/gorf24/reg/rfchan"
+	"github.com/galaktor/gorf24/reg/rfsetup"
+	"github.com/galaktor/gorf24/reg/rpd"
+	"github.com/galaktor/gorf24/reg/rxaddr"
+	"github.com/galaktor/gorf24/reg/rxpw"
+	"github.com/galaktor/gorf24/reg/status"
+	"github.com/galaktor/gorf24/reg/txaddr"
+	"github.com/galaktor/gorf24/reg/txobs"
 )
 
 const RF24_PAYLOAD_SIZE = 32
 
 type R struct {
 	/*** I/O ***/
-	spi    *spi.SPI
-	ce     *gpio.Pin
-	csn    *gpio.Pin
+	spi *spi.SPI
+	ce  *gpio.Pin
+	csn *gpio.Pin
 
 	/*** REGISTERS ***/
 	/* in order of appearance in spec for now */
-	config *reg.Config
-	autoAck *reg.AutoAck // ?new on nRF24L01+
-	enRxAddr *reg.EnabledRxAddresses
-	addrWid *reg.AddrWidths
-	retrans *reg.SetupRetrans
-	rfchan *reg.RfChannel
-	rfsetup *reg.RfSetup
-	status *reg.Status
-	trans *reg.TransObserve
-	rpd *reg.ReceivedPowerDetector // was 'CD' before nRF24L01+
-	rxAddrP0 *reg.FullXAddress
-	rxAddrP1 *reg.FullXAddress
-	rxAddrP2 *reg.PartialXAddress
-	rxAddrP3 *reg.PartialXAddress
-	rxAddrP4 *reg.PartialXAddress
-	rxAddrP5 *reg.PartialXAddress
-	txAddr *reg.FullXAddress
-	rxPwP0 *reg.RxPayloadWidth
-	rxPwP1 *reg.RxPayloadWidth
-	rxPwP2 *reg.RxPayloadWidth
-	rxPwP3 *reg.RxPayloadWidth
-	rxPwP4 *reg.RxPayloadWidth
-	rxPwP5 *reg.RxPayloadWidth
-	fifo *reg.FifoStatus
+	config   *config.C
+	autoAck  *autoack.AA // ?new on nRF24L01+
+	enRxAddr *enrxaddr.E
+	addrWid  *addrw.AW
+	retrans  *retrans.R
+	rfchan   *rfchan.R
+	rfsetup  *rfsetup.R
+	status   *status.S
+	trans    *txobs.O
+	rpd      *rpd.R // was 'CD' before nRF24L01+
+	rxAddrP0 *xaddr.Full
+	rxAddrP1 *xaddr.Full
+	rxAddrP2 *xaddr.Partial
+	rxAddrP3 *xaddr.Partial
+	rxAddrP4 *xaddr.Partial
+	rxAddrP5 *xaddr.Partial
+	txAddr   *xaddr.Full
+	rxPwP0   *rxpw.W
+	rxPwP1   *rxpw.W
+	rxPwP2   *rxpw.W
+	rxPwP3   *rxpw.W
+	rxPwP4   *rxpw.W
+	rxPwP5   *rxpw.W
+	fifo     *fifo.F
 	// ACK_PLD set by command
 	// TX_PLD set by command
 	// RX_PLD set by command
-	dynpd *reg.DynamicPayload
-	feat *reg.Feature
+	dynpd *dynpd.DP
+	feat  *feature.F
 }
 
 func New(spidevice string, spispeed uint32, cepin, csnpin uint8) (r *R, err error) {
 	r = &R{}
-	r.config = reg.NewConfig(0)
-	r.autoAck = reg.NewAutoAck(0)
-	r.enRxAddr = reg.NewEnabledRxAddresses(0)
-	r.addrWid = reg.NewAddrWidths(0)
-	r.retrans = reg.NewSetupRetrans(0)
-	r.rfchan = reg.NewRfChannel(0)
-	r.rfsetup = reg.NewRfSetup(0)
-	r.status = reg.NewStatus(0)
-	r.trans = reg.NewTransObserve(0)
-	r.rpd = reg.NewRPD(0)
-	r.rxAddrP0 = reg.NewFullRxAddress(pipe.P0, 0)
-	r.rxAddrP1 = reg.NewFullRxAddress(pipe.P1, 0)
-	r.rxAddrP2 = reg.NewPartialRxAddress(pipe.P2, r.rxAddrP1, 0)
-	r.rxAddrP3 = reg.NewPartialRxAddress(pipe.P3, r.rxAddrP1, 0)
-	r.rxAddrP4 = reg.NewPartialRxAddress(pipe.P4, r.rxAddrP1, 0)
-	r.rxAddrP5 = reg.NewPartialRxAddress(pipe.P5, r.rxAddrP1, 0)
-	r.txAddr = reg.NewTxAddress(0)
-	r.rxPwP0 = reg.NewRxPayloadWidth(pipe.P0, 0)
-	r.rxPwP1 = reg.NewRxPayloadWidth(pipe.P1, 0)
-	r.rxPwP2 = reg.NewRxPayloadWidth(pipe.P2, 0)
-	r.rxPwP3 = reg.NewRxPayloadWidth(pipe.P3, 0)
-	r.rxPwP4 = reg.NewRxPayloadWidth(pipe.P4, 0)
-	r.rxPwP5 = reg.NewRxPayloadWidth(pipe.P5, 0)
-	r.fifo = reg.NewFifoStatus(0)
-	r.dynpd = reg.NewDynamicPayload(0)
-	r.feat = reg.NewFeature(0)
+	r.config = config.New(0)
+	r.autoAck = autoack.New(0)
+	r.enRxAddr = enrxaddr.New(0)
+	r.addrWid = addrw.New(0)
+	r.retrans = retrans.New(0)
+	r.rfchan = rfchan.New(0)
+	r.rfsetup = rfsetup.New(0)
+	r.status = status.New(0)
+	r.trans = txobs.New(0)
+	r.rpd = rpd.New(0)
+	r.rxAddrP0 = rxaddr.NewFull(pipe.P0, 0)
+	r.rxAddrP1 = rxaddr.NewFull(pipe.P1, 0)
+	r.rxAddrP2 = rxaddr.NewPartial(pipe.P2, r.rxAddrP1, 0)
+	r.rxAddrP3 = rxaddr.NewPartial(pipe.P3, r.rxAddrP1, 0)
+	r.rxAddrP4 = rxaddr.NewPartial(pipe.P4, r.rxAddrP1, 0)
+	r.rxAddrP5 = rxaddr.NewPartial(pipe.P5, r.rxAddrP1, 0)
+	r.txAddr = txaddr.New(0)
+	r.rxPwP0 = rxpw.New(pipe.P0, 0)
+	r.rxPwP1 = rxpw.New(pipe.P1, 0)
+	r.rxPwP2 = rxpw.New(pipe.P2, 0)
+	r.rxPwP3 = rxpw.New(pipe.P3, 0)
+	r.rxPwP4 = rxpw.New(pipe.P4, 0)
+	r.rxPwP5 = rxpw.New(pipe.P5, 0)
+	r.fifo = fifo.New(0)
+	r.dynpd = dynpd.New(0)
+	r.feat = feature.New(0)
 
 	r.spi, err = spi.New(spidevice, 0, 8, spi.SPD_02MHz)
 	if err != nil {
@@ -115,7 +134,7 @@ func New(spidevice string, spispeed uint32, cepin, csnpin uint8) (r *R, err erro
 	return
 }
 
-func (r *R) Status() *reg.Status {
+func (r *R) Status() *status.S {
 	return r.status
 }
 
@@ -131,7 +150,7 @@ func (r *R) spiPump(c cmd.C, buf []byte) error {
 	if err != nil {
 		return err
 	}
-	r.status = reg.NewStatus(s)
+	r.status = status.New(s)
 
 	if buf != nil {
 		// pump buf data, overwriting content with returned date
@@ -217,7 +236,7 @@ func (r *R) flushRx() error {
 No Operation. Might be used to read the STATUS
 register
 */
-func (r *R) refreshStatus() (*reg.Status, error) {
+func (r *R) refreshStatus() (*status.S, error) {
 	// spiPump will update status on every cmd sent
 	err := r.spiPump(cmd.NOP, nil)
 	return r.status, err
@@ -246,7 +265,6 @@ func (r *R) toggleActivate() error {
 	// TODO: keep activated bool state, make de/activate() funcs
 	return r.spiPump(cmd.ACTIVATE, []byte{0x73})
 }
-
 
 /**********************/
 /***** OLD STUFF! *****/
