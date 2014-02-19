3cfc241719fd12c0f4b372a07bd8d5e6f8008ce8
Raphael Estrada <galaktor@gmx.de>
2014-02-19 13:00:01 +0000

    move rpd into own sub pkg

--- reg/rpd/rpd.go
+++ reg/rpd/rpd.go
@@ -1,6 +1,7 @@
 package reg
 
 import (
+	"github.com/galaktor/gorf24/reg"
 	"github.com/galaktor/gorf24/reg/addr"
 )
 
@@ -11,11 +12,11 @@ import (
    power level threshold for this bit.
    bits 7:1 reserved */
 type ReceivedPowerDetector struct {
-	R
+	reg.R
 }
 
 func NewRPD(flags byte) *ReceivedPowerDetector {
-	return &ReceivedPowerDetector{R{a: addr.RPD, flags: flags}}
+	return &ReceivedPowerDetector{reg.New(addr.RPD, flags)}
 }
 
 /* RPD (bit 0)
@@ -23,5 +24,5 @@ func NewRPD(flags byte) *ReceivedPowerDetector {
    are present in the RF channel you receive on. If the
    received power is less than -64 dBm, [sic] RDP = 0.*/
 func (r *ReceivedPowerDetector) Triggered() bool {
-	return r.flags&1 == 1
+	return r.Byte()&1 == 1
 }
