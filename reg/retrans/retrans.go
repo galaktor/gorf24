8c433d6e630cc6d2c311874fd883b7838f92f597
Raphael Estrada <galaktor@gmx.de>
2014-02-19 12:50:17 +0000

    move retrans into own sub pkg

--- reg/retrans/retrans.go
+++ reg/retrans/retrans.go
@@ -4,39 +4,40 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/galaktor/gorf24/reg"
 	"github.com/galaktor/gorf24/reg/addr"
 )
 
 type RetransDelay byte
 
 const (
-	RETRANS_u250  = RetransDelay(iota) // 0x0
-	RETRANS_u500                       // 0x1
-	RETRANS_u750                       // 0x2
-	RETRANS_u1000                      // 0x3
-	RETRANS_u1250                      // 0x4
-	RETRANS_u1500                      // 0x5
-	RETRANS_u1750                      // 0x6
-	RETRANS_u2000                      // 0x7
-	RETRANS_u2250                      // 0x8
-	RETRANS_u2500                      // 0x9
-	RETRANS_u2750                      // 0xA
-	RETRANS_u3000                      // 0xB
-	RETRANS_u3250                      // 0xC
-	RETRANS_u3500                      // 0xD
-	RETRANS_u3750                      // 0xE
-	RETRANS_u4000                      // 0xF
+	u250  = RetransDelay(iota) // 0x0
+	u500                       // 0x1
+	u750                       // 0x2
+	u1000                      // 0x3
+	u1250                      // 0x4
+	u1500                      // 0x5
+	u1750                      // 0x6
+	u2000                      // 0x7
+	u2250                      // 0x8
+	u2500                      // 0x9
+	u2750                      // 0xA
+	u3000                      // 0xB
+	u3250                      // 0xC
+	u3500                      // 0xD
+	u3750                      // 0xE
+	u4000                      // 0xF
 
 )
 
 /* SETUP_RETR
 Setup of Automatic Retransmission */
 type SetupRetrans struct {
-	R
+	reg.R
 }
 
 func NewSetupRetrans(flags byte) *SetupRetrans {
-	return &SetupRetrans{R{a: addr.SETUP_RETR, flags: flags}}
+	return &SetupRetrans{reg.New(addr.SETUP_RETR, flags)}
 }
 
 /* ARC (bits 3:0)
@@ -50,11 +51,11 @@ func (r *SetupRetrans) SetCount(c uint8) error {
 		return errors.New(fmt.Sprintf("value out of legal range: %v. Only values 0 - 15 allowed.", c))
 	}
 
-	r.flags = (r.flags & 0xF0) | c
+	r.R.Set((r.Byte() & 0xF0) | c)
 	return nil
 }
 func (r *SetupRetrans) GetCount() uint8 {
-	return r.flags & 0x0F
+	return r.Byte() & 0x0F
 }
 
 /* ARD
@@ -86,8 +87,8 @@ func (r *SetupRetrans) GetCount() uint8 {
    In 250kbps mode (even when the payload is not in ACK)
    the ARD must be 500μS or more.*/
 func (r *SetupRetrans) GetDelay() RetransDelay {
-	return RetransDelay(r.flags >> 4)
+	return RetransDelay(r.Byte() >> 4)
 }
 func (r *SetupRetrans) SetDelay(d RetransDelay) {
-	r.flags = (r.flags & 0x0F) | (byte(d) << 4)
+	r.R.Set((r.Byte() & 0x0F) | (byte(d) << 4))
 }
