3f89b43d65d696c4d891e45e5ded184fd29b1f28
Raphael Estrada <galaktor@gmx.de>
2014-02-19 22:16:13 +0000

    update renamed addrw pkg in test script

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 
 root=$(pwd)
-for pkg in ../gorf24 ./cmd ./reg ./reg/addr ./reg/addrwd ./reg/autoack ./reg/config ./reg/dynpd ./reg/enrxaddr ./reg/feature ./reg/fifo ./reg/retrans ./reg/rfchan ./reg/rfsetup ./reg/rpd ./reg/rxaddr ./reg/rxpw ./reg/status ./reg/txobs ./reg/txaddr ./reg/xaddr  ./pipe ./util ./spi ./gpio
+for pkg in ../gorf24 ./cmd ./reg ./reg/addr ./reg/addrw ./reg/autoack ./reg/config ./reg/dynpd ./reg/enrxaddr ./reg/feature ./reg/fifo ./reg/retrans ./reg/rfchan ./reg/rfsetup ./reg/rpd ./reg/rxaddr ./reg/rxpw ./reg/status ./reg/txobs ./reg/txaddr ./reg/xaddr  ./pipe ./util ./spi ./gpio
 do
 	echo "***** TESTING PACKAGE: '$pkg' *****"
 	cd $pkg
