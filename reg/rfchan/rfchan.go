b4714bbffc83a5660fb6b382bd587428ffe31aff
Raphael Estrada <galaktor@gmx.de>
2014-02-19 12:52:03 +0000

    move rfchan into own sub pkg

--- reg/rfchan/rfchan.go
+++ reg/rfchan/rfchan.go
@@ -4,32 +4,41 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/galaktor/gorf24/reg"
 	"github.com/galaktor/gorf24/reg/addr"
 )
-
-// bit 7 always zero
 
 /* RF_CH
    RF Channel
    bit 7 reserved */
 type RfChannel struct {
-	R
+	reg.R
 }
 
 func NewRfChannel(flags byte) *RfChannel {
-	return &RfChannel{R{a: addr.RF_CH, flags: flags}}
+	return &RfChannel{reg.New(addr.RF_CH, flags)}
 }
 
 /* RF_CH (bits 6:0)
    Sets the frequency channel nRF24L01+ operates on */
 func (c *RfChannel) Get() uint8 {
-	return c.flags & 0x7F // ignore bit 7
+	return c.Byte() & 0x7F // ignore bit 7
 }
 func (c *RfChannel) Set(ch uint8) error {
 	if ch > 127 {
 		return errors.New(fmt.Sprintf("value outside of legal range: %v. Only values 1 - 127 allowed.", ch))
 	}
 
-	c.flags = ch & 0x7F
+	c.R.Set(ch & 0x7F)
 	return nil
 }
+
+
+
+
+
+
+
+
+
+
