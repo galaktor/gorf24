db3eeff216f0ddd685a7648b491436694c18c780
Raphael Estrada <galaktor@gmx.de>
2014-02-19 16:53:06 +0000

    move rxpw into own sub pkg

---
+++ reg/rxpw/rxpw.go
@@ -0,0 +1,46 @@
+package rxpw
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/galaktor/gorf24/pipe"
+	"github.com/galaktor/gorf24/reg"
+	"github.com/galaktor/gorf24/reg/addr"
+)
+
+const MAX uint8 = 32
+
+/* PX_PW_Px
+   Number of bytes in RX payload in data pipe 0 (1 to
+   32 bytes).
+   0 Pipe not used
+   1 = 1 byte
+   ...
+   32 = 32 bytes
+
+   bits 7:6 reserved */
+type RxPayloadWidth struct {
+	reg.R
+}
+
+func NewRxPayloadWidth(p pipe.P, flags byte) *RxPayloadWidth {
+	return &RxPayloadWidth{reg.New(addr.RX_PW(p), flags)}
+}
+
+/* RX_PWx (bits 5:0) */
+func (r *RxPayloadWidth) Set(w uint8) error {
+	if w > MAX {
+		return errors.New(fmt.Sprintf("value out of legal range: %v. allowed values from 0 - %v", w, MAX))
+	}
+
+	r.R.Set((r.Byte() & 0) | w)
+	return nil
+}
+func (r *RxPayloadWidth) Get() uint8 {
+	if r.Byte() > MAX {
+		return MAX
+	}
+
+	return r.Byte()
+}
