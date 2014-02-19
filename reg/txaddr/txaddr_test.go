ea2f43ca03d405a49531992ae9c2d1d398f8d196
Raphael Estrada <galaktor@gmx.de>
2014-02-19 21:45:45 +0000

    txaddr test for new func

--- reg/txaddr/txaddr_test.go
+++ reg/txaddr/txaddr_test.go
@@ -2,9 +2,18 @@ package txaddr
 
 import (
 	"testing"
+
+	"github.com/galaktor/gorf24/reg/addr"
+	"github.com/galaktor/gorf24/reg/xaddr"
 )
 
-func TestTodo_I_need_tests(t *testing.T) {
-	t.FailNow()
+func TestNew_RegisterAddress_IsTX_ADDR(t *testing.T) {
+	a := New(xaddr.New(0))
+	expected := addr.TX_ADDR
+
+	actual := a.Address()
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with txaddr '%v'", expected, actual, a)
+	}
 }
-  
