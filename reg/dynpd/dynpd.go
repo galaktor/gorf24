df695f8e2e02015129e0c6ce143fe24b946d8229
Raphael Estrada <galaktor@gmx.de>
2014-02-19 12:39:10 +0000

    move dynpd into own sub pkg

--- reg/dynpd/dynpd.go
+++ reg/dynpd/dynpd.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/galaktor/gorf24/pipe"
+	"github.com/galaktor/gorf24/reg"
 	"github.com/galaktor/gorf24/reg/addr"
 )
 
@@ -12,11 +13,11 @@ import (
    Enable dynamic payload length
    bits 7:6 reserved */
 type DynamicPayload struct {
-	R
+	reg.R
 }
 
 func NewDynamicPayload(flags byte) *DynamicPayload {
-	return &DynamicPayload{R{a: addr.DYNPD, flags: flags}}
+	return &DynamicPayload{reg.New(addr.DYNPD, flags)}
 }
 
 /* DPL_Px
@@ -25,17 +26,17 @@ func NewDynamicPayload(flags byte) *DynamicPayload {
 func (d *DynamicPayload) IsEnabled(p pipe.P) (enabled bool) {
 	switch p {
 	case pipe.P0:
-		enabled = d.flags&0x01 == 0x01
+		enabled = d.Byte()&0x01 == 0x01
 	case pipe.P1:
-		enabled = d.flags&0x02 == 0x02
+		enabled = d.Byte()&0x02 == 0x02
 	case pipe.P2:
-		enabled = d.flags&0x04 == 0x04
+		enabled = d.Byte()&0x04 == 0x04
 	case pipe.P3:
-		enabled = d.flags&0x08 == 0x08
+		enabled = d.Byte()&0x08 == 0x08
 	case pipe.P4:
-		enabled = d.flags&0x10 == 0x10
+		enabled = d.Byte()&0x10 == 0x10
 	case pipe.P5:
-		enabled = d.flags&0x20 == 0x20
+		enabled = d.Byte()&0x20 == 0x20
 	}
 
 	return
@@ -52,17 +53,17 @@ func (d *DynamicPayload) enable(p pipe.P) (err error) {
 
 	switch p {
 	case pipe.P0:
-		d.flags |= 0x01
+		d.R.Set(d.Byte() | 0x01)
 	case pipe.P1:
-		d.flags |= 0x02
+		d.R.Set(d.Byte() | 0x02)
 	case pipe.P2:
-		d.flags |= 0x04
+		d.R.Set(d.Byte() | 0x04)
 	case pipe.P3:
-		d.flags |= 0x08
+		d.R.Set(d.Byte() | 0x08)
 	case pipe.P4:
-		d.flags |= 0x10
+		d.R.Set(d.Byte() | 0x10)
 	case pipe.P5:
-		d.flags |= 0x20
+		d.R.Set(d.Byte() | 0x20)
 	default:
 		err = errors.New(fmt.Sprintf("invalid pipe: %v", p))
 	}
@@ -74,17 +75,17 @@ func (d *DynamicPayload) disable(p pipe.P) (err error) {
 
 	switch p {
 	case pipe.P0:
-		d.flags &= 0xFE
+		d.R.Set(d.Byte() & 0xFE)
 	case pipe.P1:
-		d.flags &= 0xFD
+		d.R.Set(d.Byte() & 0xFD)
 	case pipe.P2:
-		d.flags &= 0xFB
+		d.R.Set(d.Byte() & 0xFB)
 	case pipe.P3:
-		d.flags &= 0xF7
+		d.R.Set(d.Byte() & 0xF7)
 	case pipe.P4:
-		d.flags &= 0xEF
+		d.R.Set(d.Byte() & 0xEF)
 	case pipe.P5:
-		d.flags &= 0xDF
+		d.R.Set(d.Byte() & 0xDF)
 	default:
 		err = errors.New(fmt.Sprintf("invalid pipe: %v", p))
 	}
