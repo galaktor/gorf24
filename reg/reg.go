ed5603cc9e3654935a046fa2ac87524f0ffa7c52
Raphael Estrada <galaktor@gmx.de>
2014-02-21 17:44:45 +0000

    reworking Reg to support built-in mask, Reader/Writer interfaces

--- reg/reg.go
+++ reg/reg.go
@@ -5,27 +5,60 @@
 package reg
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/galaktor/gorf24/reg/addr"
 )
 
 /* a register with a one-byte address and one-byte data */
 type R struct {
 	a     addr.A
+	mask  byte
 	flags byte
 }
 
-func New(a addr.A, flags byte) R {
-	return R{a, flags}
-}
+const NO_MASK byte = 0xFF // 11111111
 
-func (r *R) Byte() byte {
-	return r.flags
-}
-
-func (r *R) Set(flags byte) {
-	r.flags = flags
+func New(a addr.A, mask byte) R {
+	return R{a, mask, 0}
 }
 
 func (r *R) Address() addr.A {
 	return r.a
 }
+
+func (r *R) Mask() byte {
+	return r.mask
+}
+
+func (r *R) Get() byte {
+	return r.flags
+}
+
+func (r *R) Set(flags byte) {
+	r.flags = flags & r.mask
+}
+
+// IO.WRITER
+func (r *R) Write(p []byte) (n int, err error) {
+	if len(p) == 1 {
+		r.Set(p[0])
+		return 1,nil
+	}
+
+	return 0,errors.New(fmt.Sprintf("cannot write %v bytes. register can only store exactly 1 byte", len(p)))
+}
+
+// IO.READER
+func (r *R) Read(p []byte) (n int, err error) {
+	p[0] = r.flags
+	return 1,nil
+}
+
+
+
+
+
+
+
--- reg/reg_test.go
+++ reg/reg_test.go
@@ -6,8 +6,9 @@ package reg
 
 import (
 	"testing"
-	
+
 	"github.com/galaktor/gorf24/reg/addr"
+	"github.com/galaktor/gorf24/util"
 )
 
 func TestNew_AddressFortyTwo_StoresThatAddress(t *testing.T) {
@@ -21,44 +22,185 @@ func TestNew_AddressFortyTwo_StoresThatAddress(t *testing.T) {
 	}
 }
 
-func Test_FlagsFortyTwo_StoresThatByte(t *testing.T) {
+func TestNew_MaskFortyTwo_StoresThaMask(t *testing.T) {
 	expected := byte(42)
 	r := New(addr.A(0), expected)
 
-	actual := r.Byte()
+	actual := r.Mask()
 
 	if actual != expected {
 		t.Errorf("expected '%b' but found '%b' with reg '%v'", expected, actual, r)
 	}
 }
 
-func TestSet_FortyTwo_StoresThatByte(t *testing.T) {
+func TestNO_MASK_Is_AllOnes(t *testing.T) {
+	expected := util.B("11111111")
+
+	actual := NO_MASK
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
+
+func TestSet_FortyTwo_NoMask_StoresThatByte(t *testing.T) {
 	expected := byte(42)
-	r := New(addr.A(0), 0)
+	r := New(addr.A(0), NO_MASK)
 
 	r.Set(expected)
 
-	actual := r.Byte()
+	actual := r.flags
 	if actual != expected {
 		t.Errorf("expected '%b' but found '%b' with reg '%v'", expected, actual, r)
 	}
 }
 
+func TestSet_FortyTwo_ZeroMask_StoresZero(t *testing.T) {
+	expected := byte(0)
+	r := New(addr.A(0), 0x00)
 
+	r.Set(expected)
 
+	actual := r.flags
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with reg '%v'", expected, actual, r)
+	}
+}
 
+func TestSet_AllOnes_MixedMask_StoresMaskedValue(t *testing.T) {
+	expected := byte(0xAA)
+	r := New(addr.A(0), 0xAA)
 
+	r.Set(util.B("11111111"))
 
+	actual := r.flags
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with reg '%v'", expected, actual, r)
+	}
+}
 
+func TestGet_FortyTwo_NoMask_ReturnsFortyTwo(t *testing.T) {
+	expected := byte(42)
+	r := New(addr.A(0), NO_MASK)
+	r.flags = 42
 
+	actual := r.Get()
 
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with reg '%v'", expected, actual, r)
+	}
+}
 
+func TestWrite_OneByte_NoMask_StoresThatByte(t *testing.T) {
+	expected := byte(42)
+	r := New(addr.A(0), NO_MASK)
 
+	r.Write([]byte{expected})
 
+	actual := r.flags
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with reg '%v'", expected, actual, r)
+	}
+}
 
+func TestWrite_OneByte_Masked_StoresMaskedByte(t *testing.T) {
+	expected := byte(0xAA)
+	r := New(addr.A(0), 0xAA)
 
+	r.Write([]byte{util.B("11111111")})
 
+	actual := r.flags
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with reg '%v'", expected, actual, r)
+	}
+}
 
+func TestWrite_OneByte_ReturnsNilError(t *testing.T) {
+	r := New(addr.A(0), NO_MASK)
 
+	_,err := r.Write([]byte{0})
 
+	if err != nil {
+		t.Errorf("unexpected error: '%v'", err)
+	}
+}
 
+func TestWrite_OneByte_ReturnsOne(t *testing.T) {
+	expected := 1
+	r := New(addr.A(0), NO_MASK)
+
+	actual,_ := r.Write([]byte{0})
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with reg '%v'", expected, actual, r)
+	}
+}
+
+func TestWrite_NoBytes_DoesNotChangeFlags(t *testing.T) {
+	expected := util.B("11111111")
+	r := New(addr.A(0), NO_MASK)
+	r.Set(expected)
+
+	r.Write(make([]byte, 0))
+
+	actual := r.flags
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b' with reg '%v'", expected, actual, r)
+	}
+}
+
+func TestWrite_NoByte_ReturnsError(t *testing.T) {
+	expected := "cannot write 0 bytes. register can only store exactly 1 byte"
+	r := New(addr.A(0), NO_MASK)
+
+	_, err := r.Write(make([]byte, 0))
+
+	if err == nil {
+		t.Error("expected error but found nil")
+		t.FailNow()
+	}
+
+	actual := err.Error()
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with reg '%v'", expected, actual, r)
+	}
+}
+
+func TestWrite_TwoBytes_ReturnsError(t *testing.T) {
+	expected := "cannot write 2 bytes. register can only store exactly 1 byte"
+	r := New(addr.A(0), NO_MASK)
+
+	_, err := r.Write(make([]byte, 2))
+
+	if err == nil {
+		t.Error("expected error but found nil")
+		t.FailNow()
+	}
+
+	actual := err.Error()
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with reg '%v'", expected, actual, r)
+	}
+}
+
+func TestWrite_NoByte_ReturnsZero(t *testing.T) {
+	expected := 0
+	r := New(addr.A(0), NO_MASK)
+
+	actual, _ := r.Write(make([]byte, 0))
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with reg '%v'", expected, actual, r)
+	}
+}
+
+func TestWrite_TwoBytes_ReturnsZero(t *testing.T) {
+	expected := 0
+	r := New(addr.A(0), NO_MASK)
+
+	actual, _ := r.Write(make([]byte, 2))
+
+	if actual != expected {
+		t.Errorf("expected '%v' but found '%v' with reg '%v'", expected, actual, r)
+	}
+}
