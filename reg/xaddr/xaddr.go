6234841d649deae0f03609a6346d54faf255c319
Raphael Estrada <galaktor@gmx.de>
2014-02-25 17:40:23 +0000

    use 5bytes instead of int64, fix ToI func

--- reg/xaddr/xaddr.go
+++ reg/xaddr/xaddr.go
@@ -12,15 +12,39 @@ package xaddr
    64bits
    <-- ignored -----------><-- used ------------------------------>
    xxxxxxxxxxxxxxxxxxxxxxxx1111111111111111111111111111111111111111 */
-type A uint64
+type A [5]byte
 
-const RES_MASK uint64 = 0x000000FFFFFFFFFF
-
-func New(flags uint64) A {
-	// mask out first 3 bytes
-	return A(flags & RES_MASK)
+func New(flags [5]byte) A {
+	return A(flags)
 }
 
-func (a A) Get() uint64 {
-	return uint64(a)
+func NewFromI(flags uint64) A {
+	// iToB5 will shift out first 3 bytes, effectively masking them out
+	return New(iToB5(flags))
+}
+
+func (a A) ToB5() [5]byte {
+	return [5]byte(a)
+}
+
+func (a A) ToI() uint64 {
+	return b5ToI(a.ToB5())
+}
+
+func iToB5(bits uint64) [5]byte {
+	return [5]byte{
+		byte(bits >> 32),
+		byte(bits >> 24),
+		byte(bits >> 16),
+		byte(bits >> 8),
+		byte(bits),
+	}
+}
+
+func b5ToI(b [5]byte) uint64 {
+	return uint64(b[0]) << 32 |
+	       uint64(b[1]) << 24 |
+	       uint64(b[2]) << 16 |
+	       uint64(b[3]) << 8 |
+	       uint64(b[4])
 }
--- reg/xaddr/xaddr_test.go
+++ reg/xaddr/xaddr_test.go
@@ -9,9 +9,9 @@ import (
 )
 
 func TestNew_AllZeroes_ReturnsAllZeroes(t *testing.T) {
-	expected := A(0x0000000000)
-	
-	actual := New(0x0000000000)
+	expected := [5]byte{0x00, 0x00, 0x00, 0x00, 0x00}
+
+	actual := New([5]byte{0x00, 0x00, 0x00, 0x00, 0x00})
 
 	if actual != expected {
 		t.Errorf("expected '%b' but found '%b'", expected, actual)
@@ -19,9 +19,9 @@ func TestNew_AllZeroes_ReturnsAllZeroes(t *testing.T) {
 }
 
 func TestNew_AllOnes_ReturnsAllOnes(t *testing.T) {
-	expected := A(0xFFFFFFFFFF)
-	
-	actual := New(0xFFFFFFFFFF)
+	expected := [5]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
+
+	actual := New([5]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF})
 
 	if actual != expected {
 		t.Errorf("expected '%b' but found '%b'", expected, actual)
@@ -29,52 +29,103 @@ func TestNew_AllOnes_ReturnsAllOnes(t *testing.T) {
 }
 
 func TestNew_MixedOnesAndZeroes_ReturnsRightBits(t *testing.T) {
-	expected := A(0xAAAAAAAAAA)
-	
-	actual := New(0xAAAAAAAAAA)
+	expected := [5]byte{0xAA, 0xAA, 0xAA, 0xAA, 0xAA}
 
-
-	if actual != expected {
-		t.Errorf("expected '%b' but found '%b'", expected, actual)
-	}
-}
-
-func TestNew_OverflowFiveBytes_TruncatesToFiveBytes(t *testing.T) {
-	expected := A(0xAAAAAAAAAA)
-
-	actual := New(0xFFFFFFAAAAAAAAAA)
+	actual := New([5]byte{0xAA, 0xAA, 0xAA, 0xAA, 0xAA})
 
 	if actual != expected {
 		t.Errorf("expected '%b' but found '%b'", expected, actual)
 	}
 }
 
-func TestByte_MixedOnesAndZeroes_ReturnsRightBits(t *testing.T) {
-	expected := uint64(0xAAAAAAAAAA)
+func TestToB5_MixedOnesAndZeroes_ReturnsRightBits(t *testing.T) {
+	expected := [5]byte{0xAA, 0xAA, 0xAA, 0xAA, 0xAA}
 	a := New(expected)
 
-	actual := a.Get()
+	actual := a.ToB5()
 
 	if actual != expected {
 		t.Errorf("expected '%b' but found '%b'", expected, actual)
 	}
 }
 
+func TestToI_MixedOnesAndZeroes_ReturnsRightBits(t *testing.T) {
+	expected := uint64(0xAAAAAAAAAA)
+	a := NewFromI(expected)
+	
+	actual := a.ToI()
 
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
 
+func TestIToB5_Zeroes_ReturnsZeroes(t *testing.T) {
+	expected := [5]byte{0, 0, 0, 0, 0}
 
+	actual := iToB5(0x0000000000)
 
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
 
+func TestIToB5_Ones_ReturnsMaxBytes(t *testing.T) {
+	expected := [5]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
 
+	actual := iToB5(0xFFFFFFFFFF)
 
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
 
+func TestIToB5_MixedZeroesAndOnes_ReturnsRightBytes(t *testing.T) {
+	expected := [5]byte{0xAA, 0xAA, 0xAA, 0xAA, 0xAA}
 
+	actual := iToB5(0xAAAAAAAAAA)
 
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
 
+func TestIToB5_Overflow5Bytes_ReturnsTruncated(t *testing.T) {
+	expected := [5]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF} // 5 bytes
 
+	actual := iToB5(0xFFFFFFFFFFFF) // 6 bytes
 
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
 
+func TestB5ToI_Zeroes_ReturnsZeroes(t *testing.T) {
+	expected := uint64(0x0000000000)
 
+	actual := b5ToI([5]byte{0, 0, 0, 0, 0})
 
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
 
+func TestB5ToI_Ones_ReturnsMaxBytes(t *testing.T) {
+	expected := uint64(0xFFFFFFFFFF)
 
+	actual := b5ToI([5]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF})
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
+
+func TestB5ToI_MixedZeroesAndOnes_ReturnsRightBytes(t *testing.T) {
+	expected := uint64(0xAAAAAAAAAA)
+
+	actual := b5ToI([5]byte{0xAA, 0xAA, 0xAA, 0xAA, 0xAA})
+
+	if actual != expected {
+		t.Errorf("expected '%b' but found '%b'", expected, actual)
+	}
+}
