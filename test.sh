18b6418724ce524bd72bce737898dd74e2f895d9
Raphael Estrada <galaktor@gmx.de>
2014-07-13 15:03:36 +0100

    stdout total nr of failed tests at the end

--- test.sh
+++ test.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-
+failed=0
 root=$(pwd)
+
 for pkg in ../gorf24 ./cmd ./reg ./reg/addr ./reg/addrw ./reg/autoack ./reg/config ./reg/dynpd ./reg/enrxaddr ./reg/feature ./reg/fifo ./reg/retrans ./reg/rfchan ./reg/rfsetup ./reg/rpd ./reg/rxaddr ./reg/rxpw ./reg/status ./reg/txobs ./reg/txaddr ./reg/xaddr  ./pipe ./util ./spi ./gpio
+
 do
 	echo "***** TESTING PACKAGE: '$pkg' *****"
 	cd $pkg
 	go test -v
+	# get exit code from 'go test' IF FAILED
+	exitcode=$?
+	if [ "${exitcode}" -ne "0" ] ; then
+	    failed=$(($failed+$exitcode))
+	fi
+
 	echo ""
 	cd $root
 done
 
-# TODO: any way to join these in one single loop?
-#for file in *_test.go; do
-#    echo "testing $file"
-#    go test $file -v
-#done
+echo "************************"
+echo "  $failed  TESTS FAILED"
+echo "************************"
 
-#for file in **/*_test.go; do
-#    echo "testing $file"
-#    go test $file -v
-#done
+exit $failed
